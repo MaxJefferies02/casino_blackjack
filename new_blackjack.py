0f69b0d4af23e789ad6c454019c95b0ad909bb0e
Luke Jefferies <52199503+jefferies917@users.noreply.github.com>
2019-08-18 19:21:14 +0100

    Update new_blackjack.py

--- new_blackjack.py
+++ new_blackjack.py
@@ -1,5 +1,6 @@
 from random import shuffle
 import os
+import time
 
 # define the card ranks, and suits
 ranks = [_ for _ in range(2, 11)] + ['Jack', 'Queen', 'King', 'Ace']
@@ -19,7 +20,6 @@ shuffle(deck)
 def hit(hand):
     new_player_card = deck.pop()
     hand.append(new_player_card)
-    print(f'You Drew: {new_player_card[0]} of {new_player_card[1]}\n')
 
 
 def ascii_display(hand):
@@ -55,23 +55,61 @@ def ascii_display(hand):
         lines[10].append('└─────────┘')
     for i in range(11):
         for x in range(len(lines[i])):
-            print(lines[i][x], end='')
+            if i == 9 and x == len(lines[i])-1:
+                print(lines[i][x], end=' Hand')
+            else:
+                print(lines[i][x], end='')
         print()
-    print()
 
 
-def ascii_hidden():
-    print('┌─────────┐')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('│░░░░░░░░░│')
-    print('└─────────┘')
+def ascii_display_dealer(hand):
+    lines = [[] for i in range(11)]
+    if len(hand) == 2:
+        if hand[0][0] == 'Jack' or hand[0][0] == 'Queen' or hand[0][0] == 'King' or hand[0][0] == 'Ace':
+            label = hand[0][0].rstrip('ackuening')
+            space = ' '
+        elif hand[0][0] == 10:
+            label = '10'
+            space = ''
+        else:
+            label = hand[0][0]
+            space = ' '
+        if hand[0][1] == 'Spades':
+            symbol = '♠'
+        elif hand[0][1] == 'Hearts':
+            symbol = '♥'
+        elif hand[0][1] == 'Diamonds':
+            symbol = '♦'
+        elif hand[0][1] == 'Clubs':
+            symbol = '♣'
+        lines[0].append('┌─────────┐')
+        lines[1].append(f'│ {label}{space}      │')
+        lines[2].append('│         │')
+        lines[3].append('│         │')
+        lines[4].append('│         │')
+        lines[5].append(f'│    {symbol}    │')
+        lines[6].append('│         │')
+        lines[7].append('│         │')
+        lines[8].append('│         │')
+        lines[9].append(f'│      {space}{label} │')
+        lines[10].append('└─────────┘')
+        lines[0].append('┌─────────┐')
+        lines[1].append('│░░░░░░░░░│')
+        lines[2].append('│░░░░░░░░░│')
+        lines[3].append('│░░░░░░░░░│')
+        lines[4].append('│░░░░░░░░░│')
+        lines[5].append('│░░░░░░░░░│')
+        lines[6].append('│░░░░░░░░░│')
+        lines[7].append('│░░░░░░░░░│')
+        lines[8].append('│░░░░░░░░░│')
+        lines[9].append('│░░░░░░░░░│ Dealer Hand')
+        lines[10].append('└─────────┘')
+        for i in range(11):
+            for x in range(len(lines[i])):
+                print(lines[i][x], end='')
+            print()
+    elif len(hand) > 2:
+        ascii_display(hand)
 
 
 def clear():
@@ -97,7 +135,7 @@ def card_value(card):
 
 
 def hand_value(hand):
-    """Returns the integer value of set of cards"""
+    """Returns the integer value of the hand"""
     score = sum(card_value(card) for card in hand)
     num_aces = len([_ for _ in hand if _[0] is 'Ace'])
     while num_aces > 0:
@@ -113,20 +151,39 @@ def split():
     pass
 
 
-def bust_check():
-    pass
+def bust_check(player_hand):
+    if hand_value(player_hand) > 21:
+        print(f'You bust with a count of {hand_value(player_hand)}\n')
+        play_again()
+    elif hand_value(player_hand) == 21:
+        print('You have 21! Dealers turn\n')
+        pass
+    else:
+        pass
+    print()
+
+
+def game_state_start(dealer_hand, player_hand):
+    print('Dealer Hand: ')
+    ascii_display_dealer(dealer_hand)
+    print('Your Hand: ', end='')
+    [print(f'{player_hand[i][0]} of {player_hand[i][1]}, ', end='') for i in range(len(player_hand)-1)]
+    print(f'{player_hand[len(player_hand)-1][0]} of {player_hand[len(player_hand)-1][1]}')
+    ascii_display(player_hand)
+    print(f'Your Count: {hand_value(player_hand)}\n')
 
 
 def game_state(dealer_hand, player_hand):
-    print(f'Dealer Count: {hand_value(dealer_hand)}')
     print('Dealer Hand: ', end='')
     [print(f'{dealer_hand[i][0]} of {dealer_hand[i][1]}, ', end='') for i in range(len(dealer_hand)-1)]
-    print(f'{dealer_hand[len(dealer_hand)-1][0]} of {dealer_hand[len(dealer_hand)-1][1]}\n')
-    print(f'Your Count: {hand_value(player_hand)}')
+    print(f'{dealer_hand[len(dealer_hand)-1][0]} of {dealer_hand[len(dealer_hand)-1][1]}')
+    ascii_display(dealer_hand)
     print('Your Hand: ', end='')
     [print(f'{player_hand[i][0]} of {player_hand[i][1]}, ', end='') for i in range(len(player_hand)-1)]
-    print(f'{player_hand[len(player_hand)-1][0]} of {player_hand[len(player_hand)-1][1]}\n')
+    print(f'{player_hand[len(player_hand)-1][0]} of {player_hand[len(player_hand)-1][1]}')
     ascii_display(player_hand)
+    print(f'Your Count: {hand_value(player_hand)}')
+    print(f'Dealer Count: {hand_value(dealer_hand)}\n')
 
 
 def blackjack(hand):
@@ -138,61 +195,74 @@ def blackjack(hand):
 
 
 def play_again():
-    x = input('Do you want to play again? ')
+    x = input('Do you want to play again? (Yes = 1, No = 0) ')
     print()
-    if x.lower()[0] == 'y':
+    if x.lower()[0] == '1':
         game()
+    elif x.lower()[0] == '0':
+        clear()
+        exit()
 
 
 def game():
     clear()
+    # add the betting functionality here
+    # burn card
+    deck.pop()
     # issue the player and dealer their first two cards
     player_hand = [deck.pop(), deck.pop()]
     dealer_hand = [deck.pop(), deck.pop()]
-    print(f'You Drew: {player_hand[0][0]} of {player_hand[0][1]}, {player_hand[1][0]} of {player_hand[1][1]}\n')
-    game_state(dealer_hand, player_hand)
+    game_state_start(dealer_hand, player_hand)
+    # add insurance if ace is shown
     if blackjack(dealer_hand):
-        print('Dealer has blackjack')
-        exit()
+        print('Dealer has blackjack\n')
+        play_again()
     elif blackjack(player_hand):
-        print('You have blackjack')
-        exit()
+        print('You have blackjack\n')
+        play_again()
     # Players turn
     response = int(input('Hit or Stand? (Hit = 1, Stand = 0) '))
+    # add split and double down
     print()
     while response:
         clear()
         hit(player_hand)
-        game_state(dealer_hand, player_hand)
-        # Check if bust
-        if hand_value(player_hand) > 21:
-            print(f'You bust with a count of {hand_value(player_hand)}\n')
-            play_again()
-        elif hand_value(player_hand) == 21:
-            print('You have 21! Dealers turn')
-        else:
-            pass
+        game_state_start(dealer_hand, player_hand)
+        bust_check(player_hand)
         response = int(input('Hit or Stand? (Hit = 1, Stand = 0) '))
-        print()
     # dealers turn
     clear()
     game_state(dealer_hand, player_hand)
     # sort this logic out omg this is bad
-    # dealer rules to hit
-    while hand_value(dealer_hand) < hand_value(player_hand):
-        hit(dealer_hand)
-        # check if bust
-        if hand_value(dealer_hand) > 21:
-            print(f'Dealer bust with count of {hand_value(dealer_hand)}, You win!')
-            play_again()
+    # check if dealer wins with current hand
+    if hand_value(dealer_hand) > hand_value(player_hand):
+        print(f'You Lose! Dealer has {hand_value(dealer_hand)} and you only have {hand_value(player_hand)}\n')
+        play_again()
+    elif hand_value(dealer_hand) == hand_value(player_hand):
+        print(f'Push! You and the dealer both have {hand_value(dealer_hand)}\n')
+        play_again()
+    else:
+        
+        # dealer sticks once he gets to 17
+        while hand_value(dealer_hand) < 17:
+            time.sleep(3)
+            hit(dealer_hand)
+            clear()
+            game_state(dealer_hand, player_hand)
+            # check if bust
+            if hand_value(dealer_hand) > 21:
+                print(f'Dealer bust with count of {hand_value(dealer_hand)}, You win!\n')
+                play_again()
         # check who won
-        if hand_value(dealer_hand) >= hand_value(player_hand):
-            print(f'You Lose! Dealer has {hand_value(dealer_hand)} and you only have {hand_value(player_hand)}')
+        if hand_value(dealer_hand) > hand_value(player_hand):
+            print(f'You Lose! Dealer has {hand_value(dealer_hand)} and you only have {hand_value(player_hand)}\n')
+            play_again()
+        elif hand_value(dealer_hand) == hand_value(player_hand):
+            print(f'Push! You and the dealer both have {hand_value(dealer_hand)}\n')
             play_again()
         elif hand_value(dealer_hand) < hand_value(player_hand):
-            print(f'You Win! Dealer has {hand_value(dealer_hand)} and you have {hand_value(player_hand)}')
+            print(f'You Win! Dealer has {hand_value(dealer_hand)} and you have {hand_value(player_hand)}\n')
             play_again()
 
 if __name__ == '__main__':
     game()
-    play_again()
